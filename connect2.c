9b6e172e426c23e10a8e53086b35b384981c269b
tyrysheva.K.A <tyrysheva245@gmail.com>
2024-04-23 15:35:14 +0700

    Добавлены комментарии в программы работы с файлами

--- connect2.c
+++ connect2.c
@@ -2,22 +2,27 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+// подключение библиотеки для управления файлами 
 #include <fcntl.h>
 int main(){
-	int fd;
- 	size_t size;
-        char string[17];
+	int fd;//файловый дескриптер
+ 	size_t size;//размер строки в байтаз
+        char string[18];
+//открытие файла на чтение
         if ((fd=open("text1.txt", O_RDONLY))<0) {
                 printf("Ошибка при открытии файла на чтении\n");
                 exit(-1);
         }
-
-        size =read(fd, string, 17);
-        if(size != 17){
+//чтение из файла из файла
+        size =read(fd, string, 18);
+        //проверка кол-ва прочитанных байт
+	if(size != 18){
                 printf ("НЕ удалось прочитать всю строку из файла\n");
                 exit(-1);
                 }
+//печать строки
         printf("%s\n", string);
+//закрытие файла 
         if(close(fd)<0){
 		printf ("не удалось открыть файл\n");
 		exit(-1);
