ac4820da326665309e7d2f4bbedd0cc963ca800a
tyrysheva.K.A <tyrysheva245@gmail.com>
2024-05-14 11:33:04 +0700

    добавленны комментарии в программу работающую с  threadами

--- threads.c
+++ threads.c
@@ -2,12 +2,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int a = 0;
+int a = 0;// инициализация глобальной статической переменной, доступной из каждого thread'а 
+//функция выполняющая в рамказ второго thread'а
 void *mythread(void *dummy){
-	pthread_t mythid;
+	pthread_t mythid;//индификатор thread'a  - для каждого имеет свое  значение
 	mythid = pthread_self();
 	a = a+1;
-	printf("thread %ld,результат вычисления  a=%d\n", mythid, a);
+	printf("thread %ld,результат вычисления  a=%d\n", mythid, a);//печать индификатора текущего thread'a и значения глобальной переменной 
 	return NULL;
 }
 
@@ -15,16 +16,16 @@ int main(){
 
 	pthread_t thid, mythid;
 	int result;
-	result = pthread_create(&thid, (pthread_attr_t *)NULL, mythread, NULL);
+	result = pthread_create(&thid, (pthread_attr_t *)NULL, mythread, NULL);//создание нового thread'a при помощи вызова функции my thread()
 	if(result != 0){
 		printf("ошибка при создании нового thread\'a, возвращенное значение = %d\n", result);
 		exit(-1);
 	}
 	printf("thread создан, thid = %ld\n",thid);
-	mythid = pthread_self();
+	mythid = pthread_self();//сохраняем в переменнойц mythid индификатор главного thid'a 
 	 a = a + 1;
 	printf("thread %ld результат вычисления a=%d\n", mythid, a);
-	pthread_join(thid, (void **)NULL);
+	pthread_join(thid, (void **)NULL);//ожидание завершения порожденного threada
 	return 0;
 }
 
