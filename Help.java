565baf0f0cca0f4f334102b77f3e3a6c716a3773
tyrysheva.K.A <tyrysheva245@gmail.com>
2024-05-21 13:08:14 +0700

    Добавили обработку ошибочного ввода

--- Help.java
+++ Help.java
@@ -2,14 +2,19 @@ class Help {
 	public static void main(String args[])
 		throws java.io.IOException {
  		
-		char choice;
+		char choice, ignore;
 
-		System.out.println("Справочная система: ");
-		System.out.println(" 1. if");
-		System.out.println(" 2. switch");
-		System.out.println("Выберите подраздел: ");
-		choice = (char) System.in.read();
-	
+		do {
+			System.out.println("Справочная система: ");
+			System.out.println(" 1. if");
+			System.out.println(" 2. switch");
+			System.out.println("Выберите подраздел: ");
+			choice = (char) System.in.read();
+			do{
+				ignore = (char) System.in.read();
+			}while (ignore != '\n');
+
+		} while (choice < '1'| choice > '2');
 		System.out.println("\n");
 
 		switch (choice){
@@ -20,7 +25,7 @@ class Help {
 				break;
 			case '2':
 				System.out.println("Оператор switch:\n");
-                                System.out.println("switch(условие) {;");
+                                System.out.println("switch(условие) {");
                                 System.out.println("	case константа:");
 				System.out.println("	последовательность операторов");
                                 System.out.println("	break;");
